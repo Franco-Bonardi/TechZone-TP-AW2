5acf04ef49fff7152af77a25efd309d4db7fbfb9
Franco-Bonardi <franbonardi97@gmail.com>
2025-06-03 17:40:01 -0300

    Se solucionan bugs de rutas para la entrega 3 del TP

--- pages/login/login.js
+++ pages/login/login.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("formLogIn").addEventListener("submit", function(event) {
     event.preventDefault() // Según investigué, esto evita que el formulario se envíe para poder hacer la simulación de redirección al home
 
-    window.location.href = "../../index.html"
+    window.location.href = "/index.html"
     /* Cuando realmente vaya a enviar los datos más adelante, se elimina o comenta el event.preventDefault() y el código de redirección, y el formulario se enviará normalmente */
   })
 })
--- pages/registro/registro.js
+++ pages/registro/registro.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("formRegistro").addEventListener("submit", function(event) {
     event.preventDefault() // Según investigué, esto evita que el formulario se envíe para poder hacer la simulación de redirección al login
 
-    window.location.href = "../login/login.html"
+    window.location.href = "/pages/login/login.html"
     /* Cuando realmente vaya a enviar los datos más adelante, se elimina o comenta el event.preventDefault() y el código de redirección, y el formulario se enviará normalmente */
   })
 })

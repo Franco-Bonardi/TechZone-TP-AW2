545cbb635f50b8c4292836e4e3be875bc3f6b986
Franco-Bonardi <franbonardi97@gmail.com>
2025-06-16 12:47:13 -0300

    Se agrega archivo JSON para productos y se modifican detalles visuales, para entrega 4 del TP

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 import { navbarComponent } from "./components/navbarIndex.js";
-import { cardComponent } from "./components/cardIndex.js";
 
 let navContainer = document.querySelector('header');
-let cardContainers = document.querySelectorAll('.cardContainer');
 
 window.addEventListener('load', () => {
     navContainer.innerHTML = navbarComponent
 
-    cardContainers.forEach(container => {
-        container.innerHTML = cardComponent
-    })
+    cargarProductos('destacados')
 })
+
+function cargarProductos (categoria) {
+    fetch(`./data/productos.json`)
+    .then(res => res.json())
+    .then(data => {
+        const contenedor = document.getElementById("tarjeta")
+        contenedor.innerHTML = ""
+
+        const productosFiltrados = data.filter( producto => producto.category === categoria )
+
+        productosFiltrados.forEach(producto => {
+            contenedor.innerHTML += `
+            <div class="col">
+                <div class="card h-100 d-flex flex-column p-3">
+                    <div class="img-container">
+                        <img src="${producto.image}" class="card-img-top" alt="${producto.name}" />
+                    </div>
+                    <div class="card-body d-flex flex-column justify-content-end">
+                        <h5 class="card-title titulo-prod">${producto.name}</h5>
+                        <p class="card-text">${producto.description}</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row text-center">
+                            <div class="col">
+                                <p class="price"><strong>$${producto.price}</strong></p>
+                            </div>
+                            <div class="col">
+                                <input type="number" class="form-control" min="1" placeholder="1">
+                            </div>
+                            <div class="col">
+                                <button class="btn btn-success"><i class="bi bi-bag-plus-fill"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+        })
+    })
+}
--- pages/pages.js
+++ pages/pages.js
@@ -1,13 +1,57 @@
 import { navbarComponent } from "../components/navbar.js";
-import { cardComponent } from "../components/card.js";
 
 let navContainer = document.querySelector('header');
-let cardContainers = document.querySelectorAll('.cardContainer');
 
 window.addEventListener('load', () => {
     navContainer.innerHTML = navbarComponent
 
-    cardContainers.forEach(container => {
-        container.innerHTML = cardComponent
-    })
+    const bodyId = document.body.id
+
+    if (bodyId === 'componentes') {
+        cargarProductos('componentes')
+    } else if (bodyId === 'PCs') {
+        cargarProductos('PCs')
+    } else if (bodyId === 'perifericos') {
+        cargarProductos('perifericos')
+    }
 })
+
+function cargarProductos (categoria) {
+    fetch(`../../data/productos.json`)
+    .then(res => res.json())
+    .then(data => {
+        const contenedor = document.getElementById("tarjeta")
+        contenedor.innerHTML = ""
+
+        const productosFiltrados = data.filter( producto => producto.category === categoria )
+
+        productosFiltrados.forEach(producto => {
+            contenedor.innerHTML += `
+            <div class="col">
+                <div class="card h-100 d-flex flex-column p-3">
+                    <div class="img-container">    
+                        <img src="${producto.image}" class="card-img-top" alt="${producto.name}" />
+                    </div>
+                    <div class="card-body d-flex flex-column justify-content-end">
+                        <h5 class="card-title titulo-prod">${producto.name}</h5>
+                        <p class="card-text">${producto.description}</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row text-center">
+                            <div class="col">
+                                <p class="price"><strong>$${producto.price}</strong></p>
+                            </div>
+                            <div class="col">
+                                <input type="number" class="form-control" min="1" placeholder="1">
+                            </div>
+                            <div class="col">
+                                <button class="btn btn-success"><i class="bi bi-bag-plus-fill"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+        })
+    })
+}
